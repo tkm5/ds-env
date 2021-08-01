cb8ca657b8b2f502e695742b13393acce69745f1
tkm5 <005tbg@gmail.com>
2021-08-02 01:58:21 +0900

    update others & make ctvis

---
+++ docker/work/basic/ctvis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from glob import glob
+
+def _get_df(base_path='covid-data', folder='rp_im'):
+    data_dict = pd.DataFrame({'FilePath': glob(f'{base_path}/{folder}/*'),
+                              'FileName': [p.split('/')[-1] for p in glob(f'{base_path}/{folder}/*')]})
+    return data_dict
+
+def get_df_all(base_path='covid-data'):
+    rp_im_df = _get_df(base_path, folder='rp_im')
+    rp_msk_df = _get_df(base_path, folder='rp_msk')
+    return rp_im_df.merge(rp_msk_df, on='FileName', suffixes=('Image', 'Mask'))
